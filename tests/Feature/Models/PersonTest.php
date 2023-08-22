34759c5243da81a804ed5108c439158abed76b0a
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-08-22 20:03:07 +0700

    Belajar Attribute Casting laravel eloquent

--- tests/Feature/Models/PersonTest.php
+++ tests/Feature/Models/PersonTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Models;
 
 use App\Models\Person;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -26,6 +27,18 @@ class PersonTest extends TestCase
         self::assertEquals('ZAHWA', $person->first_name);
         self::assertEquals('amira', $person->last_name);
         self::assertEquals('ZAHWA amira', $person->full_name);
+
+    }
+    public function testAttributeCasting(): void
+    {
+        $person = new Person();
+        $person->first_name = 'Daud';
+        $person->last_name = 'Ramadhan';
+        $person->save();
+        self::assertNotNull($person->created_at);
+        self::assertNotNull($person->updated_at);
+        self::assertInstanceOf(Carbon::class, $person->created_at);
+        self::assertInstanceOf(Carbon::class, $person->updated_at);
 
     }
 }
